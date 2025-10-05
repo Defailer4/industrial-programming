af9e67b425d4aeea27b5ee34d0cac60b50ff7a3b
D3ffailer <100451419+Defailer4@users.noreply.github.com>
2025-10-05 16:58:48 +0300

    Update 2.cpp

--- 06.10/2.cpp
+++ 06.10/2.cpp
@@ -1,1 +1,24 @@
-//222
+ #include <iostream>
+
+ using namespace std;
+
+ int main() {
+    int N, K;
+    cout << "Введите N: ";
+    cin >> N;
+    int Q = N*2;
+    int arr[Q];
+    cout << "Введите порядок очереди из " << N << " человек:" << endl;
+    for (int i = 0; i < N; i++ ){
+        cin >> arr[i];
+        arr[i+5] = arr[i];
+    }
+    cout << "Введите количество пуговиц: ";
+    cin >> K;
+    while (K > N){
+        K -= N;
+    }
+    for (int i = N-K; i < Q - K; i++ ){
+        cout << arr[i] << " ";
+    }
+}
