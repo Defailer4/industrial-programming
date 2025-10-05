6679efbfd5a6272387385a6efc1d624344e427f7
D3ffailer <100451419+Defailer4@users.noreply.github.com>
2025-10-05 20:11:19 +0300

    Update 4.cpp

--- 06.10/4.cpp
+++ 06.10/4.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    int N;
+    cin >> N;
+    
+    int total = 0;
+    
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            if (i + j == N - 1) {
+                cout << 1;
+                total += 1;
+            } else if (i + j > N - 1) {
+                cout << 2;
+                total += 2;
+            } else {
+                cout << 0;
+            }
+        }
+        cout << endl;
+    }
+    
+    cout << total << endl;    
+    return 0;
+}
