a282533c9aae92ae49e77ce89f383f5cd1b00531
D3ffailer <100451419+Defailer4@users.noreply.github.com>
2025-10-05 20:21:20 +0300

    Update 5.cpp

--- 06.10/5.cpp
+++ 06.10/5.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    int N;
+    cin >> N;
+    
+    int result = 0;
+    for (int i = 0; i < N; i++) {
+        int x;
+        cin >> x;
+        result = result^x;
+    }
+    
+    cout << result << endl;
+    
+    return 0;
+}
