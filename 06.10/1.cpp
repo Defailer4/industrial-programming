298d7f991b4f24da483f94b082d77f7069b65122
D3ffailer <100451419+Defailer4@users.noreply.github.com>
2025-10-05 15:12:54 +0300

    Update 1.cpp

--- 06.10/1.cpp
+++ 06.10/1.cpp
@@ -1,1 +1,22 @@
-111
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int N;
+    cout << "Введите N: ";
+    cin >> N;
+    int arr[N];
+    cout << "Введите " << N << " целых чисел:" << endl;
+    for (int i = 0; i < N; i++) {
+        cin >> arr[i];
+    }
+    int counter = 0;
+    for (int i = 0; i < N; i++) {
+        if (arr[i] > 16){
+            counter += 1;
+        }
+    }
+    cout << "По правилу не проходят " << counter << " человек" << endl;
+    return 0;
+}
