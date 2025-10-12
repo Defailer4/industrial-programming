7e3c3ee4034cb75d7672ef095e9e0ef1df019289
D3ffailer <100451419+Defailer4@users.noreply.github.com>
2025-10-12 12:47:06 +0300

    Update 4.cpp

--- 13.10/4.cpp
+++ 13.10/4.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+using namespace std;
 
+long long stepen(int Num, int PowOfNum){
+    long long Answer;
+    Answer = Num;
+    while(PowOfNum != 1){
+        Answer *= Num;
+        PowOfNum -= 1;
+    }
+    return Answer;
+}
+
+int main(){
+    int Num, PowOfNum;
+    cout << "Введите число и степень:" << endl;
+    cin >> Num >> PowOfNum;
+    cout << stepen(Num, PowOfNum);
+    return 0;
+}
