228095499aec17f69ee59a34c4c18291b36aabdb
D3ffailer <100451419+Defailer4@users.noreply.github.com>
2025-10-12 15:46:27 +0300

    Update 5.cpp

--- 13.10/5.cpp
+++ 13.10/5.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+using namespace std;
 
+void transfer(int N, int from, int second, int to) {
+    if (N == 1) {
+        cout << "Ingredient " << N << " move from " << from << " to " << to << endl;
+        return;
+    }
+    
+    transfer(N - 1, from, to, second);
+    cout << "Ingredient " << N << " move from " << from << " to " << to << endl;
+    transfer(N - 1, to, second, from);
+}
+
+int main() {
+    int N;
+    cin >> N;
+    
+    transfer(N, 1, 2, 3);
+    
+    return 0;
+}
