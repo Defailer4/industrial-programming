8f5b7cf351bb688a7525792bcd7b5776cc804f54
D3ffailer <100451419+Defailer4@users.noreply.github.com>
2025-11-07 22:20:22 +0300

    Update 4.cpp

--- 10.11/4.cpp
+++ 10.11/4.cpp
@@ -1,1 +1,32 @@
-www
+#include <iostream>
+#include <stack>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string ss;
+    stack<char> stroki;
+    bool Answer = true;
+
+    cout<<"Введите строку: ";
+    getline(cin,ss);
+
+    for (int i = 0; i < ss.length();i++){
+        if (ss[i] == '('){
+            stroki.push(ss[i]);
+        }else if(ss[i] == ')'){
+            if (stroki.empty()){
+                Answer = false;
+                break;
+            }
+            stroki.pop();
+
+        }
+    }
+    if (!stroki.empty()){
+        Answer = false;
+    }
+    cout<< (Answer ? "yes" : "no")<<endl;
+    return 0;
+}   
