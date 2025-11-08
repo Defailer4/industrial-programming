103c739772e04bf4531d944edc785401cf5a758e
D3ffailer <100451419+Defailer4@users.noreply.github.com>
2025-11-08 09:53:37 +0300

    Update 5.cpp

--- 10.11/5.cpp
+++ 10.11/5.cpp
@@ -1,1 +1,42 @@
-www
+#include <iostream>
+#include <vector>
+#include <set>
+#include <string>
+
+using namespace std;
+
+int main() {
+    cout<<"ПРОГРАММА РАБОТАЕТ ТОЛЬКО С АНГЛ СЛОВАМИ (Я НЕ ПРИДУМАЛ РЕАЛИЗАЦИЮ)"<<endl;
+    string text;
+    cout << "Введите текст: ";
+    getline(cin, text);
+    
+    vector<string> result;
+    set<string> checker;
+    string word = "";
+    
+    for (int i = 0; i <= text.size(); i++) {
+        char c = (i < text.size()) ? text[i] : ' ';
+        
+        if (c >= 'A' && c <= 'Z') c += 32;
+        else if (c >= 'А' && c <= 'Я') c += 32;
+        else if (c == 'Ё') c = 'ё';
+        
+        if ((c >= 'a' && c <= 'z') || (c >= 'а' && c <= 'я') || c == 'ё' || c == '-') {
+            word += c;
+        } else if (!word.empty()) {
+            if (checker.find(word) == checker.end()) {
+                result.push_back(word);
+                checker.insert(word);
+            }
+            word = "";
+        }
+    }
+    
+    cout << "Уникальные слова:"<<endl;
+    for (const string& w : result) {
+        cout << w << endl;
+    }
+    
+    return 0;
+}
